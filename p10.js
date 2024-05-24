23014234e949b7534b9008026249b0df8c3d01a9
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-05-24 23:14:22 +0600

    solve another way

--- p10.js
+++ p10.js
@@ -3,9 +3,16 @@
 const numbers = [6, 78, 45, 34, 9];
 
 const findMaxValue = () => {
-  const result = numbers.sort((a, b) => b - a);
-  const max = result[0]
+  //   const result = numbers.sort((a, b) => b - a);
+  //   const max = result[0]
+  //   console.log(max);
+  let max = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+  }
   console.log(max);
 };
 
-findMaxValue()
+findMaxValue();
