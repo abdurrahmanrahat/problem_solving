a58a9d1acd5a0208c7dae41f1fa96e92f93adc47
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-05-24 23:32:12 +0600

    problem 11

---
+++ p11.js
@@ -0,0 +1,34 @@
+// Create An Array Of Objects Representing Students With 'Name' And 'Grades' Properties. Write A Function To Sort The Students By Average Grade In Descending Order.
+
+const students = [
+  {
+    name: "Alice",
+    grades: [85, 92, 88, 96],
+  },
+  {
+    name: "Bob",
+    grades: [79, 82, 91, 89],
+  },
+  {
+    name: "Charlie",
+    grades: [92, 90, 85, 87],
+  },
+];
+
+const sortByResultAverage = () => {
+  const makeAverage = (numbers) => {
+    const totalMark = numbers.reduce((sum, num) => {
+      return sum + num;
+    }, 0);
+
+    const average = totalMark / (numbers.length + 1);
+    return average;
+  };
+
+  const sortStudents = students.sort(
+    (a, b) => makeAverage(b.grades) - makeAverage(a.grades)
+  );
+  console.log(sortStudents);
+};
+
+sortByResultAverage();
